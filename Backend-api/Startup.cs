e5132f8cb5835f2476e6ecf3625ee20f9a96201f
GhostersH <97006291+GhostersH@users.noreply.github.com>
2023-05-13 22:30:55 -0500

    Update Startup.cs

--- Backend-api/Startup.cs
+++ Backend-api/Startup.cs
@@ -1,56 +1,59 @@
-
-using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System.Net.Http;
-using System.Web.Http;
-
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-namespace TuNombreDeProyecto
-{
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowSpecificOrigin", builder =>
-                {
-                    builder.WithOrigins("https://login-proyecto-angular-master-2.vercel.app") // Aquí va la URL de tu aplicación Angular  
-                           .AllowAnyHeader()
-                           .AllowAnyMethod();
-                });
-            });
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseRouting();
-
-            app.UseCors("AllowSpecificOrigin");
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
-}
+using Microsoft.AspNetCore.Builder;  
+using Microsoft.AspNetCore.Hosting;  
+using Microsoft.Extensions.Configuration;  
+using Microsoft.Extensions.DependencyInjection;  
+using Microsoft.Extensions.Hosting;  
+  
+namespace TuNombreDeProyecto  
+{  
+    public class Startup  
+    {  
+        public IConfiguration Configuration { get; }  
+  
+        public Startup(IConfiguration configuration)  
+        {  
+            Configuration = configuration;  
+        }  
+  
+        public void ConfigureServices(IServiceCollection services)  
+        {  
+            services.AddControllers();  
+  
+            services.AddCors(options =>  
+            {  
+                options.AddPolicy("AllowSpecificOrigin",  
+                    builder =>  
+                    {  
+                        builder.WithOrigins("https://login-proyecto-angular-master-2.vercel.app")  
+                            .AllowAnyHeader()  
+                            .AllowAnyMethod();  
+                    });  
+            });  
+        }  
+  
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)  
+        {  
+            if (env.IsDevelopment())  
+            {  
+                app.UseDeveloperExceptionPage();  
+            }  
+            else  
+            {  
+                app.UseExceptionHandler("/Error");  
+            }  
+  
+            app.UseHttpsRedirection();  
+  
+            app.UseRouting();  
+  
+            app.UseCors("AllowSpecificOrigin");  
+  
+            app.UseAuthorization();  
+  
+            app.UseEndpoints(endpoints =>  
+            {  
+                endpoints.MapControllers();  
+            });  
+        }  
+    }  
+}  
